bc0b88f339695399adf73ac1ec4a12e78a9a1d20
danieli-santos <dannysantos1106@gmailcom>
2023-02-27 20:56:30 -0300

    Operadores

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,74 @@
+import org.xml.sax.ext.EntityResolver2;
+
+public class Operadores {
+    // public static void main(String[] args) {
+    //     String nomeCompleto = "LINGUAGEM" + " JAVA";
+    //     System.out.println(nomeCompleto);
+    // }
+
+    // public static void main(String[] args){
+
+    //     String concatenacao ="?"; 
+
+    //         concatenacao = 1+1+1+"1";
+    //         System.out.println(concatenacao);
+
+    //         concatenacao = 1+"1"+1+1;
+    //         System.out.println(concatenacao);
+
+    //         concatenacao = 1+"1"+1+"1";
+    //         System.out.println(concatenacao);
+
+    //         concatenacao = "1"+1+1+1;
+    //         System.out.println(concatenacao);
+
+    //         concatenacao = "1"+(1+1+1);
+    //         System.out.println(concatenacao);
+
+    // }
+
+    // public static void main(String[] args) {
+    //     int numero = 5;
+        
+    //     numero = - numero ;
+    //     System.out.println(numero); 
+
+    //     numero = numero * -1;
+
+    //     System.out.println(numero);
+        
+    // }
+
+
+    // public static void main(String[] args) {
+    //     int numero = 5;
+    //     //x repeticao
+    //     // numero = numero + 2;
+    //     //  System.out.println(numero);
+
+    //     //  numero++;
+    //      System.out.println( -- numero);
+
+    //      System.out.println(numero);   
+    // }
+
+
+    public static void main(String[] args) {
+        int a, b;
+        
+        a = 5;
+        b = 6;
+
+        // String resultado = "";
+        // if(a==b)
+        //     resultado = "verdadeiro";
+        // else
+        //     resultado = "falso";
+
+        //     System.out.println(resultado);
+
+        String resultado = a==b ? "verdadeiro" : "falso"; // precisa ser um op boleana
+         System.out.println(resultado);
+
+    }
+}
