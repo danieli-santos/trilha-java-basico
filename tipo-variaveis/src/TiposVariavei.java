6926fbc2a1912954964af586d308ef17f0e1b64a
danieli-santos <danielisantos1106@gmail.com>
2023-02-27 16:15:20 -0300

    alteracao

--- tipo-variaveis/src/TiposVariavei.java
+++ tipo-variaveis/src/TiposVariavei.java
@@ -1,5 +1,8 @@
+import org.ietf.jgss.Oid;
+
 public class TiposVariavei {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("oi");
     }
 }
