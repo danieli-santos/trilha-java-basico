f0289fea6c61843d6d35d001ec8db31bc03f046a
danieli-santos <dannysantos1106@gmailcom>
2023-02-27 16:47:20 -0300

    tipos

--- tipo-variaveis/src/TiposVariavei.java
+++ tipo-variaveis/src/TiposVariavei.java
@@ -2,7 +2,18 @@ import org.ietf.jgss.Oid;
 
 public class TiposVariavei {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Ola mundo");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // final < para o valor não ser auterado, e deve ser escrito em caixa alta
+
+        
+        
     }
 }
