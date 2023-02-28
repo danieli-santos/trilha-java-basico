cdce2bbf79308d89cbcd84dfa844508cc4f359d1
danieli-santos <dannysantos1106@gmail.com>
2023-02-27 21:30:57 -0300

    op logicos

--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -53,11 +53,11 @@ public class Operadores {
     // }
 
 
-    public static void main(String[] args) {
-        int a, b;
+  //  public static void main(String[] args) {
+ //       int a, b;
         
-        a = 5;
-        b = 6;
+  //      a = 5;
+ //       b = 6;
 
         // String resultado = "";
         // if(a==b)
@@ -67,8 +67,58 @@ public class Operadores {
 
         //     System.out.println(resultado);
 
-        String resultado = a==b ? "verdadeiro" : "falso"; // precisa ser um op boleana
-         System.out.println(resultado);
+  //      String resultado = a==b ? "verdadeiro" : "falso"; // precisa ser um op boleana
+  //       System.out.println(resultado);
 
+   // }
+
+       // public static void main(String[] args) {
+            //String nomeUm = "Danieli";
+            //String nomeDois = "Danieli";
+
+           // String nomeUm = "Danieli";
+           // String nomeDois = new String("Danieli");// caso quando é dois objetos em memoria, nesse caso devemos usar o equals
+
+          //  System.out.println(nomeUm == nomeDois);
+
+
+
+
+
+           // int numero1 = 1;
+           // int numero2 = 2;
+            
+           // boolean simNao = numero1 == numero2;
+
+           // if (numero1 == numero2){
+           //     System.out.println("a nossa condiçao é verdadeira");
+           // }
+
+           // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+          //  simNao = numero1 != numero2;
+
+           // System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+
+           // simNao = numero1 > numero2;
+
+           // System.out.println("numeroUm é maior que numeroDois? " + simNao);
+   // }
+
+   public static void main(String[] args) {
+    boolean condicao1 = true;
+
+    boolean condicao2 = false;
+
+    if(condicao1 && (7 > 4)){
+        System.out.println("as duas condiçôes sâo verdadeiras");
     }
+
+    if (condicao1 || condicao2) {
+        System.out.println("um da condiçôes sâo verdadeiras");
+        
+    }
+    System.out.println("fim");
+
+   }
 }
