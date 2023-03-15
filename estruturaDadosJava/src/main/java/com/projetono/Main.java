db982e188d64b19629db3aaa3010b094238a01a6
danieli-santos <dannysantos1106@gmail.com>
2023-03-14 22:25:52 -0300

    projetono

--- estruturaDadosJava/src/main/java/com/projetono/Main.java
+++ estruturaDadosJava/src/main/java/com/projetono/Main.java
@@ -3,13 +3,13 @@ package com.projetono;
 public class Main {
     public static void main(String[] args) {
 
-        No no1 = new No("conteudo no1");
-        No no2 = new No("conteudo no2");
+        No<String> no1 = new No<String>("conteudo no1");
+        No<String> no2 = new No<String>("conteudo no2");
         no1.setProximoNo(no2);
 
-        No no3 = new No("conteudo no3");
+        No<String> no3 = new No<String>("conteudo no3");
         no2.setProximoNo(no3);
-        No no4 = new No("conteudo no4");
+        No<String> no4 = new No<String>("conteudo no4");
         no3.setProximoNo(no4);
 
         //no1>no2>no3>no4>null;
