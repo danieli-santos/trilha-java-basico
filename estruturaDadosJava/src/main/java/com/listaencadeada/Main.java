8abc6f09c3a3a46b0551a6bb73a0b5035d0e557d
danieli-santos <dannysantos1106@gmail.com>
2023-03-16 21:58:20 -0300

    lista encadeada

--- estruturaDadosJava/src/main/java/com/listaencadeada/Main.java
+++ estruturaDadosJava/src/main/java/com/listaencadeada/Main.java
@@ -1,2 +1,24 @@
-package com.listaencadeada;public class Main {
+package com.listaencadeada;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ListaEncadeada<String> minhaListaEncadada = new ListaEncadeada<String>();
+        minhaListaEncadada.add("teste1");
+        minhaListaEncadada.add("teste2");
+        minhaListaEncadada.add("teste3");
+        minhaListaEncadada.add("teste4");
+
+
+        System.out.println(minhaListaEncadada.get(0));
+        System.out.println(minhaListaEncadada.get(1));
+        System.out.println(minhaListaEncadada.get(2));
+        System.out.println(minhaListaEncadada.get(3));
+
+        System.out.println(minhaListaEncadada);
+
+        System.out.println(minhaListaEncadada.remove(3));
+        System.out.println(minhaListaEncadada);
+
+    }
 }
