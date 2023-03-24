db53c9909f8a2d808c08cde5e0614e965345f5d2
danieli-santos <dannysantos1106@gmail.com>
2023-03-23 22:40:56 -0300

    Desafios

--- desafios/src/main/java/desafios/imprimindopositivoemedia/Main.java
+++ desafios/src/main/java/desafios/imprimindopositivoemedia/Main.java
@@ -1,2 +1,66 @@
-package desafios.imprimindopositivoemedia;public class Main {
-}
+package desafios.imprimindopositivoemedia;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int cont = 0;
+        double media = 0;
+        //double valorePositivos = 0;
+        double[] numeros = {2,-2, 3, -5, 7, 9};
+        //double numeros;// = leitor.nextDouble();
+
+        //TODO: Implemente as condições adequadas para obter a quantidade
+        //de valores positivos e a média dos valores positivos:
+
+        for (int i = 0; i < numeros.length; i++) {
+            //System.out.println("numeros: ");
+            //numeros = leitor.nextDouble();
+            if (numeros[i] > 0) {
+                cont++;
+                media += numeros[i];
+            }
+        }
+        media = media /= cont;
+        System.out.println(cont + " valores positivos");
+        System.out.println(String.format("%.1f", media));
+        }
+
+
+    }
+//Como foi resolvido na DIO
+
+//public class DIO {
+//
+//    public static void main(String[] args) throws IOException {
+//        Scanner leitor = new Scanner(System.in);
+//        int cont = 0;
+//        double media = 0;
+//        int tamanho = 6;
+//        double[] numeros = new double[tamanho];
+//
+//        //TODO: Implemente as condições adequadas para obter a quantidade
+//        //de valores positivos e a média dos valores positivos:
+//
+//        for (int i = 0; i < tamanho; i++) {
+//            numeros[i] = leitor.nextDouble();
+//            // //numeros = leitor.nextDouble();
+//            if (numeros[i] > 0) {
+//                cont++;
+//                media += numeros[i];
+//            }
+//        }
+//
+//        media = media /=cont;
+//        System.out.println(cont + " valores positivos");
+//        System.out.println(String.format("%.1f", media));
+//    }
+
+    // media = media + cont/cont;
+    // System.out.println(cont + " valores positivos");
+    // System.out.println(String.format("%.1f", media));
+//}
+
+
--- desafios/src/main/java/desafios/somaHcomNtermos/Main.java
+++ desafios/src/main/java/desafios/somaHcomNtermos/Main.java
@@ -1,2 +1,23 @@
-package desafios.somaHcomNtermos;public class Main {
+package desafios.somaHcomNtermos;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] Args) {
+
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+
+        double h = 0;
+        for (int i = 1; i <= n; i++) {
+            //TODO: Calcule o valor de H de forma que resulte na soma dos termos:
+            h += (double) 1/i;
+        }
+        //TODO: Imprima a soma dos termos, considerando um resultado com duas casas decimais:
+        System.out.printf("%.0f", h);
+
+    }
 }
+
+
