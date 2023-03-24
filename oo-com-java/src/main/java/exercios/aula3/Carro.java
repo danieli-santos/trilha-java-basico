4af8b55ecfd6148d2a6b06a7206c7799246fdf49
danieli-santos <dannysantos1106@gmail.com>
2023-03-24 16:03:30 -0300

    exercicios OO

--- oo-com-java/src/main/java/exercios/aula3/Carro.java
+++ oo-com-java/src/main/java/exercios/aula3/Carro.java
@@ -1,4 +1,48 @@
-package exercio1classe;
+package exercios.aula3;
 
 public class Carro {
+
+    //Atributos
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    Carro(){
+
+    }
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    double totalValoTanque(double valorCambustivel){
+        return capacidadeTanque * valorCambustivel;
+    }
+
 }
--- oo-com-java/src/main/java/exercios/aula3/RodarAplicacao.java
+++ oo-com-java/src/main/java/exercios/aula3/RodarAplicacao.java
@@ -1,2 +1,27 @@
-package exercios.aula3;public class RodarAplicacao {
+package exercios.aula3;
+
+public class RodarAplicacao {
+
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+
+        carro1.setCor("Azul");
+        carro1.setModelo("BMW Series 3");
+        carro1.setCapacidadeTanque(59);
+
+
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.totalValoTanque(6.39));
+
+        Carro carro2 = new Carro("Cinza", "Mercedes-Benz Class C ", 66);
+
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.totalValoTanque(6.49));
+
+
+    }
 }
